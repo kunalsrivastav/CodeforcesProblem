30b91e5246db25e68f8b42be5b408b1bffdbccbe
Kunal Srivastav <sri2001kunal@gmail.com>
2021-02-18 23:17:43 +0530

    Watermelon.java Successfully Executed.

---
+++ src/Watermelon.java
@@ -0,0 +1,22 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 18-02-2021
+ *   Time: 23:16
+ *   File: Watermelon.java
+ */
+
+import java.util.Scanner;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte num = sc.nextByte();
+        boolean possible = num % 2 == 0;
+        if (possible && num != 2) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
