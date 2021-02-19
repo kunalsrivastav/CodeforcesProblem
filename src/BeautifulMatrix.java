382a976b510d32fb8fb2e306f2a7c6e419278368
Kunal Srivastav <sri2001kunal@gmail.com>
2021-02-19 23:52:34 +0530

    BeautifulMatrix.java Successfully Executed.

---
+++ src/BeautifulMatrix.java
@@ -0,0 +1,28 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 19-02-2021
+ *   Time: 23:47
+ *   File: BeautifulMatrix.java
+ */
+
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[5][5];
+        int iPosition = 0, jPosition = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                int num = sc.nextInt();
+                matrix[i][j] = num;
+                if (num == 1) {
+                    iPosition = i;
+                    jPosition = j;
+                }
+            }
+        }
+        int countSteps = Math.abs(iPosition - 2) + Math.abs(jPosition - 2);
+        System.out.println(countSteps);
+    }
+}
