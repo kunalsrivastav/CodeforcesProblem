5f29ab5a1299f1fd0d60b6beb5610eacbe2c91fb
Kunal Srivastav <sri2001kunal@gmail.com>
2021-02-19 23:30:29 +0530

    StringTask.java Successfully Executed.

---
+++ src/StringTask.java
@@ -0,0 +1,20 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kunal Srivastav (kunalsrivastav)
+ *   Date: 19-02-2021
+ *   Time: 23:21
+ *   File: StringTask.java
+ */
+
+import java.util.Scanner;
+
+public class StringTask {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != 'a' && s.charAt(i) != 'e' && s.charAt(i) != 'i' && s.charAt(i) != 'o' && s.charAt(i) != 'u' && s.charAt(i) != 'y') {
+                System.out.print("." + s.charAt(i));
+            }
+        }
+    }
+}
