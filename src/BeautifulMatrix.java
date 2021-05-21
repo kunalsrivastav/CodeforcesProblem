b76f3e4ac2c7fdd5b9006b80a0d3e0faad14586f
Kunal Srivastav <kunalsrivastavgs2001@gmail.com>
2021-05-21 19:57:31 +0530

    updated BeautifulMatrix.java

--- src/BeautifulMatrix.java
+++ src/BeautifulMatrix.java
@@ -7,6 +7,10 @@
 
 import java.util.Scanner;
 
+/**
+ * Question - https://codeforces.com/problemset/problem/263/A
+ * */
+
 public class BeautifulMatrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
