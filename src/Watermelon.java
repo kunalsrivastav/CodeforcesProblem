305c3eea1e5ecc3f05d158a3c2b9b3b3688676e9
Kunal Srivastav <kunalsrivastavgs2001@gmail.com>
2021-05-21 19:48:11 +0530

    updated Watermelon.java

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -7,6 +7,10 @@
 
 import java.util.Scanner;
 
+/**
+ * Question - https://codeforces.com/problemset/problem/4/A
+ * */
+
 public class Watermelon {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
